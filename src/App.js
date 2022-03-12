a2d5e35b749229fb67089c3ecee10b1c059137ea
impedaka <qinfengyu123@gmail.com>
2022-03-12 16:29:39 -0500

    alan ai

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Request from "./Request";
 import VideoCall from "./VideoCall";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Request />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/request" element={<Request />} />
           <Route path="/videocall" element={<VideoCall />} />
         </Routes>
       </BrowserRouter>
--- src/Request.js
+++ src/Request.js
@@ -1,7 +1,28 @@
-import React from "react";
-
+import { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 const Request = () => {
-  return <div>Request</div>;
+  const [People, setPeople] = useState([]);
+  useEffect(() => {
+    alanBtn({
+      key: "b27e9005ab11a695031e484fd000264d2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        if (commandData.command === "getPeople") {
+          // Call the client code that will react to the received command
+          setPeople(commandData.data);
+          console.log(commandData.data);
+        }
+      },
+    });
+  }, []);
+  return (
+    <div>
+      {People.map((People) => (
+        <li key={People.name}>
+          {People.name}-{People.service}-{People.availability}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Request;
